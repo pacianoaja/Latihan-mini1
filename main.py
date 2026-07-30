f0d78b9a43548907a07deca315ef98c23fc51535
pacianoaja <ragatpaciano9@gmail.com>
2026-07-31 00:48:41 +0700

    feat: add pipeline execution metrics and runtime tracking

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from pipeline.transformer import DataTransformer
 from pipeline.loader import PostgresLoader
 from config.database import get_db_connection
 from pipeline.logger import logger
+import time
 
 def run_validation_query():
     """
@@ -40,7 +41,7 @@ def running():
     logger.info("==================================================")
     logger.info("Memulai Pipeline E-Commerce Competitive Intelligence...")
     logger.info("==================================================")
-
+    start_time = time.perf_counter()
     
     try :
         # Target URL latihan
@@ -50,6 +51,7 @@ def running():
         extractor = PlaywrightExtractor(base_url=TARGET_URL)
         raw_products = extractor.extract_products(limit=20)
         logger.info(f"[EXTRACT] Terambil {len(raw_products)} raw record.")
+        total_extracted = len(raw_products)
         
         # 2. Transformation Layer
         transformed_products = []
@@ -57,20 +59,42 @@ def running():
             clean_data = DataTransformer.transform_product(raw)
             transformed_products.append(clean_data)
         logger.info(f"[TRANSFORM] Berhasil membersihkan {len(transformed_products)} record.")
+        total_cleaned = len(transformed_products)
         
         # 3. Loading Layer (PostgreSQL UPSERT)
         loader = PostgresLoader()
         loader.upsert_products(transformed_products)
         logger.info(f"[LOAD] Berhasil UPSERT {len(transformed_products)} record ke database.")
+        total_upserted = loader.upsert_products(transformed_products)
         
         # 4. Validation Layer
         run_validation_query()
+        data_loss = total_extracted - total_upserted
+        data_loss_rate = (data_loss / total_extracted) * 100 if total_extracted > 0 else 0.0
         
         logger.info("Pipeline selesai dieksekusi tanpa error.")
 
     except Exception as e:
         # Menangkap error fatal yang dilempar (raise) dari layer mana pun
         logger.critical(f"Pipeline berhenti secara paksa karena terjadi kesalahan fatal: {e}", exc_info=True)
+
+    end_time = time.perf_counter()
+
+    # buat nyari waktu eksekusi 
+
+    duration_seconds = end_time - start_time
+    summary_log = f"""
+==================================================
+             PIPELINE EXECUTION SUMMARY           
+==================================================
+Status           : SUCCESS
+Total Extracted  : {total_extracted} records
+Total Cleaned    : {total_cleaned} records
+Total UPSERT     : {total_upserted} records
+Data Loss        : {data_loss} records ({data_loss_rate:.2f}%)
+Total Duration   : {duration_seconds:.2f} seconds
+=================================================="""
+    logger.info(summary_log)
 
 if __name__ == "__main__":
     running()
